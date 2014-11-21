16f57e26b95465115dc98165276e2e7749111440
Andrew Walker <woxorz@gmail.com>
2014-11-21 14:06:28 -0800

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,12 @@ var gulp_shell = require('gulp-shell');
 gulp.task('default', gulp_shell.task([
 	'php build.php'
 ]));
+
+gulp.task('watch', function() {
+	gulp.watch([
+		'data/*',
+		'css/*',
+		'templates/*',
+		'*.php'
+	], ['default'])
+});
