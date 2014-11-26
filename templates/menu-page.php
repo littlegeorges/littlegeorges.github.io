e1ff85cb8f2c15d730d7f0fa753c87f1d8d58947
Andrew Walker <woxorz@gmail.com>
2014-11-26 14:23:31 -0800

    Shrinking the jump menu with the font sizes

--- templates/menu-page.php
+++ templates/menu-page.php
@@ -3,7 +3,7 @@
 	<div class="stack md-table-cell md-w-1-4 pad-left-md sidebar-background">
 		<ul id="lg-jump-menu" class="stack lg-jump-menu">
 			<?php foreach ($location->menu as $i => $menu_section): ?>
-				<li class="stack lg-jump-menu_item">
+				<li class="stack lg-jump-menu_item fs-mdsm lg-fs-md">
 					<a
 						class="lg-jump-menu_link"
 						href="#<?php echo url_slug($menu_section->title); ?>"
@@ -13,31 +13,31 @@
 				</li>
 			<?php endforeach ?>
 			<li class="stack lg-jump-menu_item pad-v-sm">
-				<h2 class="cs-4 fs-md pad-bottom-sm text-center">
+				<h2 class="cs-4 pad-bottom-sm text-center fs-mdsm">
 					DAILY PICK-UP SPECIALS
 				</h2>
 				<div class="tiles">
-					<div class="tile w-1-3 text-center">
+					<div class="tile w-1-3 text-center fs-mdsm">
 						SMALL<br>
 						$7.95<br>
 						<span class="fs-sm">2 Toppings</span>
 					</div>
-					<div class="tile w-1-3 text-center">
+					<div class="tile w-1-3 text-center fs-mdsm">
 						MEDIUM<br>
 						$9.95<br>
 						<span class="fs-sm">2 Toppings</span>
 					</div>
-					<div class="tile w-1-3 text-center">
+					<div class="tile w-1-3 text-center fs-mdsm">
 						LARGE<br>
 						$12.95<br>
 						<span class="fs-sm">2 Toppings</span>
 					</div>
 				</div>
 
-				<h2 class="cs-4 fs-md text-center pad-top-sm">
+				<h2 class="cs-4 text-center pad-top-sm fs-mdsm">
 					Tuesday Night is Pasta Night
 				</h2>
-				<p class="text-center">
+				<p class="text-center fs-mdsm">
 					Pick-up or Dine-in $7.95
 				</p>
 			</li>
