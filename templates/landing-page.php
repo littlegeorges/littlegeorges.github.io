9a15db04acdde77f99d8207f5a63a75e32c83132
Andrew Walker <woxorz@gmail.com>
2015-04-17 12:31:40 -0700

    Landing page interior shots

--- templates/landing-page.php
+++ templates/landing-page.php
@@ -31,6 +31,7 @@
 		<hr>
 		<p>Here at Little George’s, we serve a vast array of delectable Mediterranean-style foods like cannelloni, fettuccine, lasagna, spaghetti and more. We also serve tender steaks, juicy burgers, tasty chicken, and the most delicious, fall-off-the-bone ribs in town.</p>
 		<p>Are you a salad lover? We have a variety of salads featuring the freshest ingredients, including our Chef’s Salad, Caesar Salad, Teriyaki Chicken Caesar Salad, and more.</p>
+		<img class="w-fill" src="images/interior-collage.jpg" alt="">
 		<p>We’re a small, family-owned restaurant serving generous portions, along with a wide variety of beverages (alcoholic and non-alcoholic). That’s why Little George’s restaurant has become a treasured dining spot in Nanaimo where locals love to congregate.</p>
 		<p>Offering dine in, take out or delivery, stop by Little George’s for great-tasting food today!</p>
 	</div>
